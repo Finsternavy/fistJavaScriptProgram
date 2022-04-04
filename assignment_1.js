7b4a702a88b4fa1b33fe1bb11e20cfd4fcdae985
Christopher Finster <finsternavy@gmail.com>
2022-04-04 09:11:49 -0500

    assignment_1_final

--- assignment_1.js
+++ assignment_1.js
@@ -27,8 +27,10 @@ document.write(`
         <img src="img/youtube_logo.png" alt="">
     </div>
     <div class="center-menu">
-        <input class="hide-mobile" type="text">
-        <a class="spy-glass-icon" href=""><i class="fa-solid fa-magnifying-glass"></i></a>
+        <div class="search-bar">
+            <input class="hide-mobile" type="text">
+            <a class="spy-glass-icon" href=""><i class="fa-solid fa-magnifying-glass"></i></a>
+        </div>
         <a class="mic-icon hide-mobile" href=""><i class="fa-solid fa-microphone"></i></a>
     </div>
     <div class="right-menu">
@@ -38,65 +40,144 @@ document.write(`
         <a class="user-icon" href=""><i class="fa-solid fa-user"></i></a>
     </div>
 </nav>
-<div class="video">
-    <div class="video-img">
-        <img src="img/YouTube_main_video_img.png" alt="">
-    </div>
-    <div class="video-info">
-        <h3>#Asmongold</h3>
-        <h2>${videoTitle}</h2>
-    </div>
-    <div class="views-button-bar-container">
-        <div class="views-container">
-            <p>${views} views - ${datePublished}</p>
+<div class="desktop">
+    <div class="desktop-left">
+        <div class="video">
+            <div class="video-img">
+                <img src="img/YouTube_main_video_img.png" alt="">
+            </div>
+            <div class="video-info">
+                <h3>#Asmongold</h3>
+                <h2>${videoTitle}</h2>
+            </div>
+            <div class="views-button-bar-container">
+                <div class="views-container">
+                    <p>${views} views - ${datePublished}</p>
+                </div>
+                <div class="button-bar">
+                    <a href="#"><i class="fa-solid fa-thumbs-up"></i> ${likes}K</a>
+                    <a href="#"><i class="fa-solid fa-thumbs-down"></i> ${dislikes}</a>
+                    <a href="#"><i class="fa-solid fa-share"></i> SHARE</a>
+                    <a href="#"><i class="fa-solid fa-download"></i> DOWNLOAD</a>
+                    <a href="#"><i class="fa-solid fa-scissors"></i> CLIP</a>
+                    <a href="#"><i class="fa-solid fa-plus"></i> SAVE</a>
+                    <a href="#"><i class="fa-solid fa-plus"></i> ...</a>
+                </div>
+            </div>
         </div>
-        <div class="button-bar">
-            <a href="#"><i class="fa-solid fa-thumbs-up"></i> ${likes}K</a>
-            <a href="#"><i class="fa-solid fa-thumbs-down"></i> ${dislikes}</a>
-            <a href="#"><i class="fa-solid fa-share"></i> SHARE</a>
-            <a href="#"><i class="fa-solid fa-download"></i> DOWNLOAD</a>
-            <a href="#"><i class="fa-solid fa-scissors"></i> CLIP</a>
-            <a href="#"><i class="fa-solid fa-plus"></i> SAVE</a>
-            <a href="#"><i class="fa-solid fa-plus"></i> ...</a>
+
+        <div class="author-wrapper">
+            <div class="user-img">
+                <img src="img/asmongold_user_img.png" alt="">
+            </div>
+            <div class="video-description">
+                <p>${userName}</p>
+                <p>${subscribers}M subscribers</p>
+                <p class="main-description">${vidDescription}</p>
+            </div>
+        </div>
+
+        <div class="comments">
+            <p>${numComments} Comments</p>
+        </div>
+        <div class="comment-wrapper">
+            <div class="comment-user-image">
+                <img src="img/comment_user_img.png" alt="">
+            </div>
+            <div class="comment-content">
+                <p>${commentUserName}</p>
+                <p>${commentText}</p>
+                <p><i class="fa-solid fa-thumbs-up"></i> ${thumbsUp} <i class="fa-solid fa-thumbs-down"></i> ${thumbsDown}</p>
+                <p>View ${numReplies} replies</p>
+            </div>
         </div>
     </div>
-</div>
-
-<div class="author-wrapper">
-    <div class="user-img">
-        <img src="img/asmongold_user_img.png" alt="">
-    </div>
-    <div class="video-description">
-        <p>${userName}</p>
-        <p>${subscribers}M subscribers</p>
-        <p class="main-description">${vidDescription}</p>
-    </div>
-</div>
-
-<div class="comments">
-    <p>${numComments} Comments</p>
-</div>
-<div class="comment-wrapper">
-    <div class="comment-user-image">
-        <img src="img/comment_user_img.png" alt="">
-    </div>
-    <div class="comment-content">
-        <p>${commentUserName}</p>
-        <p>${commentText}</p>
-        <p><i class="fa-solid fa-thumbs-up"></i> ${thumbsUp} <i class="fa-solid fa-thumbs-down"></i> ${thumbsDown}</p>
-        <p>View ${numReplies} replies</p>
-    </div>
-</div>
-<div class="ad-container">
-    <div class="ad-image">
-        <img src="img/YouTube_ad_video.png" alt="lvndmark video">
-        <p class="video-length-label">${(videoLength - videoLength%60) / 60}:${videoLength%60}</p>
-    </div>
-    <div class="video-content">
-        <p>${videoAdName}</p>
-        <p>${videoAdUserName}</p>
-        <p>${videoAdViews}K views - ${videoAdTimeSincePost} hours ago</p>
-        <p>${videoAdTag}</p>
+    <div class="desktop-right">
+        <div class="ad-container">
+            <div class="ad-image">
+                <img src="img/YouTube_ad_video.png" alt="lvndmark video">
+                <p class="video-length-label">${(videoLength - videoLength%60) / 60}:${videoLength%60}</p>
+            </div>
+            <div class="video-content">
+                <p>${videoAdName}</p>
+                <p>${videoAdUserName}</p>
+                <p>${videoAdViews}K views - ${videoAdTimeSincePost} hours ago</p>
+                <p>${videoAdTag}</p>
+            </div>
+        </div>
+        <div class="ad-container">
+            <div class="ad-image">
+                <img src="img/YouTube_ad_video.png" alt="lvndmark video">
+                <p class="video-length-label">${(videoLength - videoLength%60) / 60}:${videoLength%60}</p>
+            </div>
+            <div class="video-content">
+                <p>${videoAdName}</p>
+                <p>${videoAdUserName}</p>
+                <p>${videoAdViews}K views - ${videoAdTimeSincePost} hours ago</p>
+                <p>${videoAdTag}</p>
+            </div>
+        </div>
+        <div class="ad-container">
+            <div class="ad-image">
+                <img src="img/YouTube_ad_video.png" alt="lvndmark video">
+                <p class="video-length-label">${(videoLength - videoLength%60) / 60}:${videoLength%60}</p>
+            </div>
+            <div class="video-content">
+                <p>${videoAdName}</p>
+                <p>${videoAdUserName}</p>
+                <p>${videoAdViews}K views - ${videoAdTimeSincePost} hours ago</p>
+                <p>${videoAdTag}</p>
+            </div>
+        </div>
+        <div class="ad-container">
+            <div class="ad-image">
+                <img src="img/YouTube_ad_video.png" alt="lvndmark video">
+                <p class="video-length-label">${(videoLength - videoLength%60) / 60}:${videoLength%60}</p>
+            </div>
+            <div class="video-content">
+                <p>${videoAdName}</p>
+                <p>${videoAdUserName}</p>
+                <p>${videoAdViews}K views - ${videoAdTimeSincePost} hours ago</p>
+                <p>${videoAdTag}</p>
+            </div>
+        </div>
+        <div class="ad-container">
+            <div class="ad-image">
+                <img src="img/YouTube_ad_video.png" alt="lvndmark video">
+                <p class="video-length-label">${(videoLength - videoLength%60) / 60}:${videoLength%60}</p>
+            </div>
+            <div class="video-content">
+                <p>${videoAdName}</p>
+                <p>${videoAdUserName}</p>
+                <p>${videoAdViews}K views - ${videoAdTimeSincePost} hours ago</p>
+                <p>${videoAdTag}</p>
+            </div>
+        </div>
+        <div class="ad-container">
+            <div class="ad-image">
+                <img src="img/YouTube_ad_video.png" alt="lvndmark video">
+                <p class="video-length-label">${(videoLength - videoLength%60) / 60}:${videoLength%60}</p>
+            </div>
+            <div class="video-content">
+                <p>${videoAdName}</p>
+                <p>${videoAdUserName}</p>
+                <p>${videoAdViews}K views - ${videoAdTimeSincePost} hours ago</p>
+                <p>${videoAdTag}</p>
+            </div>
+        </div>
+        <div class="ad-container">
+            <div class="ad-image">
+                <img src="img/YouTube_ad_video.png" alt="lvndmark video">
+                <p class="video-length-label">${(videoLength - videoLength%60) / 60}:${videoLength%60}</p>
+            </div>
+            <div class="video-content">
+                <p>${videoAdName}</p>
+                <p>${videoAdUserName}</p>
+                <p>${videoAdViews}K views - ${videoAdTimeSincePost} hours ago</p>
+                <p>${videoAdTag}</p>
+            </div>
+        </div>
+        
     </div>
 </div>
 </header>
